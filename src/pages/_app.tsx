b0d1739b38946f99348463724f50ea8967ac6418
Joaquin <joaquincaicedo96@gmail.com>
2023-04-25 18:08:04 -0500

    second try

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import '../styles.css';
 import '../styles/fonts.css';
 import { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps}/>
+      <Analytics/>
+    </>
+  );
 }
 
 export default MyApp;
